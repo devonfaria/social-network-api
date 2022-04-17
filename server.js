a6da99e43d016a490dca636548f6da4f0ede10b4
Devon Faria <devonfaria@gmail.com>
2022-04-17 14:42:39 -0400

    created basic routes, and created basic controller files for routing

--- server.js
+++ server.js
@@ -6,15 +6,16 @@ const routes = require('./routes');
 const db = require('./config/connection');
 
 // Setting up Express PORT
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
-
-app.listen(PORT, () => {
-  console.log(`App listening at http://localhost:${PORT}`);
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+  });
 });
