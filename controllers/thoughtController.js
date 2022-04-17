e494448c500d20038328ce6f92d35d145fadd333
Devon Faria <devonfaria@gmail.com>
2022-04-17 15:13:33 -0400

    created the controller functions for find all, find one, and create for Users and Thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,13 +2,26 @@ const Thought = require('../models/Thought');
 
 module.exports = {
   getAllThoughts(req, res) {
-
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
   },
   getSingleThought(req, res) {
-
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select('-__v')
+      .then((thought) => {
+        if (!thought) {
+          res.status(404).json({ message: 'No thought found' })
+        } else {
+          res.json(thought)
+        }
+      })
+      .catch((err) => res.status(500).json(err));
   },
   createThought(req, res) {
-
+    Thought.create(req.body)
+      .then((newThoughtData) => res.json(newThoughtData))
+      .catch((err) => res.status(500).json(err));
   },
   updateThought(req, res) {
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,13 +2,26 @@ const User = require('../models/User');
 
 module.exports = {
   getAllUsers(req, res) {
-
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
   },
   getSingleUser(req, res) {
-
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .then((user) => {
+        if (!user) {
+          res.status(404).json({ message: 'No user found' })
+        } else {
+          res.json(user)
+        }
+      })
+      .catch((err) => res.status(500).json(err));
   },
   createUser(req, res) {
-
+    User.create(req.body)
+      .then((newUserData) => res.json(newUserData))
+      .catch((err) => res.status(500).json(err));
   },
   updateUser(req, res) {
 
